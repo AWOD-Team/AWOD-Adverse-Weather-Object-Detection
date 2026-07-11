bf1093ec311336dce3a2f7e79b6a8fee34acf35f
函钩 <tjdx1225230685@tju.edu.cn>
2026-07-11 20:44:18 +0800

    添加了卡车

--- yolo_baseline/main.py
+++ yolo_baseline/main.py
@@ -7,7 +7,7 @@ import cv2
 import numpy as np
 from ultralytics import YOLO
 
-TARGET_CLASSES = {0: "person", 1: "bicycle", 2: "car", 3: "motorcycle", 5: "bus"}
+TARGET_CLASSES = {0: "person", 1: "bicycle", 2: "car", 3: "motorcycle", 5: "bus",7:"truck"}
 
 IMAGE_EXTS = {".jpg", ".jpeg", ".png", ".bmp", ".tiff", ".tif"}
 
